83a12397e8321933735c1ef0b2c3a05d84665202
mariored22 <33031919+mariored22@users.noreply.github.com>
2018-05-02 12:11:37 -0500

    setinterval
    re render component

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,35 +9,34 @@ class Main extends Component{
     this.props.userlist();
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (this.props.users !== nextProps.users) {
+  // componentWillReceiveProps(nextProps) {
+  //   if (this.props.users !== nextProps.users) {
 
-      clearTimeout(this.timeout);
+  //     clearTimeout(this.timeout);
 
-      // Optionally do something with data
+  //     // Optionally do something with data
 
-      if (!nextProps.isFetching) {
-        this.startPoll();
-      }
-    }
+  //     if (!nextProps.isFetching) {
+  //       this.startPoll();
+  //     }
+  //   }
+  // }
+  // componentWillUnmount() {
+  //   clearTimeout(this.timeout);
+  // }
+  // startPoll() {
+  //   this.timeout = setTimeout(() => this.props.userlist(), 15000);
+  // }
 
+// componentDidMount(){
+// if(this.props.userlist()){
+//   this.interval = setInterval(() => {
+//         this.props.userlist()
 
-  }
+//   }, 30000);
+// }
 
-  componentWillMount() {
-    this.props.userlist();
-  }
-
-  componentWillUnmount() {
-    clearTimeout(this.timeout);
-  }
-
-  startPoll() {
-    this.timeout = setTimeout(() => this.props.userlist(), 15000);
-  }
-
-
-
+//  }
 
   list(val){
     const icons = val.symbol.toLowerCase();
